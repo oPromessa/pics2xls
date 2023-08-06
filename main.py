dec3afb39797ad4a8d6e765b9e9684d094b3a7d5
oPromessa <mario@silvapereira.com>
2023-08-06 20:31:09 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from openpyxl import Workbook
+from openpyxl.drawing.image import Image
+from PIL import Image as PILImage
+
+def insert_picture_to_excel(row, worksheet):
+    img_path = row['path to a picture JPEG']
+    img = PILImage.open(img_path)
+    img = Image(img)
+    img.width = 100  # Set the desired image width in the cell
+    img.height = 100  # Set the desired image height in the cell
+    worksheet.column_dimensions['F'].width = 25  # Adjust the width of the 'remarks' column
+    worksheet.add_image(img, f'F{row.name + 2}')  # Adjust the cell where the image will be inserted
+
+def csv_to_excel_with_images(csv_file_path, excel_file_path):
+    df = pd.read_csv(csv_file_path)
+
+    # Create a new Excel workbook and add a worksheet
+    workbook = Workbook()
+    worksheet = workbook.active
+
+    # Write the CSV data to the Excel worksheet
+    for index, row in df.iterrows():
+        worksheet.append(row.tolist())
+
+        # Insert the picture in the cell of the 'path to a picture JPEG' column
+        insert_picture_to_excel(row, worksheet)
+
+    # Save the Excel workbook to the specified file
+    workbook.save(excel_file_path)
+
+if __name__ == "__main__":
+    csv_file_path = "input.csv"  # Replace with the path to your CSV file
+    excel_file_path = "output.xlsx"  # Replace with the desired output Excel file path
+
+    csv_to_excel_with_images(csv_file_path, excel_file_path)
